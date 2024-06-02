705ed135616a8f644344f2228963a8d6776db804
JackyZhang00 <qdzhangjiaqi@126.com>
2024-06-02 20:41:55 +0800

    草稿-研究与会议

--- draft.py
+++ draft.py
@@ -14,6 +14,7 @@ class Draft():
         self.players = players
         self.planets = {}
         self.actionList = {}
+        self.researchList = {'A':"",'B':"",'C':"",'D':"",'E':"",'F':""}
         while True:
             try:
                 yourNumber = int(input("你的编号为："))
@@ -32,6 +33,12 @@ class Draft():
                 self.planets[i].append('C')
         for i in range(1,players+1):
             self.actionList[i] = []
+        # 初始化研究列表
+        if self.level == 12:
+            self.researchList['X1'] = []
+        elif self.level == 18:
+            self.researchList['X1'] = []
+            self.researchList['X2'] = []
     
     def showDraftMap(self):
         result = ""
@@ -90,4 +97,35 @@ class Draft():
                     print("\t",end='')
             print("")
 
+    def initResearch(self,notes):
+        researchIndex = ['A','B','C','D','E','F','X1']
+        if self.level == 18:
+            researchIndex.append('X2')
+        assert len(notes) == len(researchIndex), "数量不相等"
+        for i in range(len(notes)):
+            self.researchList[researchIndex[i]] = notes[i]+":"
+    
+    def showResearch(self):
+        for i in self.researchList.keys():
+            print(f"{i}: {self.researchList[i]}")
 
+    def addResearch(self,index,result):
+        keys = self.researchList.keys()
+        assert index in keys, "没有该编号研究"
+        if self.researchList[index][-1] != ":":
+            choice = input("当前研究已存在，是否要将其替换？[Y]/N：")
+            if choice in ['N','n']:
+                pass
+            else:
+                title = self.researchList[index].split(":")[0]
+                self.researchList[index] = title+":"+result
+        else:
+            self.researchList[index] = self.researchList[index]+result
+
+# draft = Draft(12,4)
+# draft.initResearch(['Q1','Q2','Q3','Q4','Q5','Q6','Q7'])
+# draft.showResearch()
+# draft.addResearch('A','Result1')
+# draft.showResearch()
+# draft.addResearch('A','Result2')
+# draft.showResearch()
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from Draft import Draft
 
-draft = Draft(12,2)
+while True:
+    level = int(input("请输入难度 12 or 18: "))
+    if level in (12,18):
+        draft = Draft(level,2)
+        break
+    else:
+        print("输入错误，请重新输入")
+
+researchIndex = ['A','B','C','D','E','F','X1']
+if level == 18:
+    researchIndex.append('X2')
+
+researchList = []
+for i in researchIndex:
+    researchList.append(input("请输入"+i+"的研究项目："))
+
+draft.initResearch(researchList)
+
 while True: 
     allowList = [1,2,3]
-    x = int(input("请输入指令：\n 1 - 查看当前草稿\n 2 - 删除候选项\n 3 - 推断扇区\n 4 - 添加行动\n 5 - 查看行动\n 0 - 退出\n : "))
+    x = int(input("请输入指令：\n 1 - 查看当前草稿\n 2 - 删除候选项\n 3 - 推断扇区\n 4 - 添加行动\n 5 - 查看行动\n 6 - 查看研究与会议列表\n 7 - 进行研究\n 0 - 退出\n : "))
     if x == 1:
         draft.showDraftMap()
     elif x == 2:
@@ -35,6 +52,19 @@ while True:
 
     elif x == 5:
         draft.showActionList()
+
+    elif x == 6:
+        draft.showResearch()
+    elif x == 7:
+        while True:
+            index = input("请输入欲研究的项目")
+            result = input("请输入研究结果")
+            try:
+                draft.addResearch(index,result)
+            except:
+                print("输入错误，请重新输入")
+            else:
+                break
     elif x == 0:
         choice = input("程序将退出（不保存！），确定要退出吗？[N]/Y：")
         if choice in ['Y','y']:
